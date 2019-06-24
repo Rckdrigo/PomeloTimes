4c398f84d749f8fbb5bc065ad80b8d7a1c158a43
Rodrigo <rodrigo@eibiz.co.th>
2019-06-24 13:53:42 +0700

    improving the get method and the delete

--- server/webUserManagment/handler.js
+++ server/webUserManagment/handler.js
@@ -9,15 +9,16 @@ const headers = {
 }
 
 const dbConfig = (context) => {
-    let c = null;
-    if (context.functionName.includes('-prod-'))
-        c = config.prod
-    else if (context.functionName.includes('-test-'))
-        c = config.test
-    else
-        c = config.dev
 
-    return c;
+    if(context.awsRequestId.includes('offline'))
+        return config.offline
+
+    if (context.functionName.includes('-prod-'))
+        return config.prod
+    else if (context.functionName.includes('-test-'))
+        return config.test
+    else 
+        return config.dev
 }
 
 const concatProductRoles = (productRoles) => { 
@@ -36,6 +37,8 @@ module.exports.createSuperUser = async (event, context, callback) => {
 
     let db = dbConfig(context);
     let mysql = require('serverless-mysql')({ config: db })
+
+    console.log(event)
 
     switch(event.triggerSource){
         case 'PostConfirmation_ConfirmForgotPassword':
@@ -72,28 +75,81 @@ module.exports.webUser = async (event, context) => {
     let mysql = require('serverless-mysql')({ config: db })
 
     let response = null;
+
     switch (event.method) {
         case "GET":
             var { username } = event.path;
-            let select = 'call selectAllUserInformation(?);';
+            let selectAllUserInformation = 'call selectAllUserInformation(?);';
             
-
-            response = await mysql.query(select, [username]);      
-                // .catch(e => { throw (e) })
-
+            response = await mysql.query(selectAllUserInformation, [username]);      
             await mysql.end().catch(e => { throw (e) });
+
+            if(response[0].length === 0)
+                throw(new Error('The user does not exists.'))
+
+            let userInformation = {
+                idUser: response[0][0].idUser,
+                username: response[0][0].username,
+                firstName: response[0][0].firstName,
+                lastName: response[0][0].lastName,
+                email: response[0][0].email
+            }
+
+            let companyInformation = {
+                idCompany: response[0][0].idCompany,
+                companyName: response[0][0].companyName,
+            }
+
+            let isAuroraSuperuser =  response[0].filter(e =>{ 
+                delete e.idUser;
+                delete e.username;
+                delete e.firstName;
+                delete e.lastName;
+                delete e.email;
+                delete e.idCompany;
+                delete e.companyName;
+                return e.idRole === 1 && e.idProduct === 1
+            }).length > 0;
+
+            let allUsersResponse;
+            let allUsers = [];
+            if(isAuroraSuperuser)
+                {
+                    let getAllCompanyUsers = 'call selectAllCompanyUsers(?,?);';
+                    allUsersResponse = await mysql.query(getAllCompanyUsers, [userInformation.idUser, companyInformation.idCompany]);      
+                    await mysql.end().catch(e => { throw (e) });
+
+                    allUsersResponse[0].map(user => {
+                        let userIndex = allUsers.findIndex(u => u.username === user.username);
+                        let p = {productName: user.productName, roleName: user.roleName}
+
+                        if(allUsers[userIndex] !== undefined){
+                            allUsers[userIndex].product.push(p);
+                        }
+                        else{
+                            let u = {username : user.username, product: [] }
+                            u.product.push(p)
+                            allUsers.push(u)
+                        }
+
+                    })
+                }
 
             return {
                 headers,
                 statusCode: 200,
-                response: response[0],  
-                username,
-                db
+                response: {
+                    userInformation,
+                    companyInformation,
+                    isAuroraSuperuser,
+                    allUsers : isAuroraSuperuser ? allUsers : undefined,
+                    productRoles: response[0] 
+                }
             }
 
         case "POST":
             var { username, firstName, lastName, email, idCompany, superuser, productRoles } = event.body;
-            var create = 'call createWebUser(?,?,?,?,?,?,?);';
+            var create = 'call updateWebUser(?,?,?,?,?,?,?);';
             response = await mysql.query(create, [username, firstName, lastName, email, idCompany, superuser, concatProductRoles(productRoles)])
 
             await mysql.end()
@@ -104,8 +160,18 @@ module.exports.webUser = async (event, context) => {
                 statusCode: 200
                 // response: response[0]
             };
-    }
-    
 
+        case "DELETE":
+                var { username } = event.path;
+                var create = 'call deleteWebUser(?);';
+                response = await mysql.query(create, [username])
+    
+                await mysql.end()
+                    .catch(e => { throw (e) })
+                return {
+                    headers,
+                    statusCode: 200
+                };
+    }
 
 }
